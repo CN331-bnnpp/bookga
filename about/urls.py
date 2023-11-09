0b48f084866bfedf34a32401fb59a3a6241e395e
6510615138 <6510615138@student.tu.ac.th>
2023-11-09 19:33:56 +0700

    startapp staff

--- about/urls.py
+++ about/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('about', views.about, name='about'),
     path('', views.home, name='home'),
-    path('/en', views.home, name='home'),
+    path('en', views.home, name='home'),
 ]
