fb939b4f3c31d9c80c4a614b08a19a37f631ffc3
party <6410685215@student.tu.ac.th>
2023-11-10 13:10:35 +0700

    Update login and signup views and templates

--- account/views.py
+++ account/views.py
@@ -2,10 +2,12 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.views.decorators.csrf import csrf_exempt
 from django.contrib import messages
-from .forms import LoginForm
+from .forms import SignUpForm
 from .models import User, group, group_member
 
 # Create your views here.
+def index(request):
+    return render(request, "account/user.html")
 @csrf_exempt
 def login(request):
     if request.user.is_authenticated:
@@ -13,8 +15,11 @@ def login(request):
         return render_login(request, user)
     
     if request.method == "GET":
-        loginForm = LoginForm()
-        return render(request, "login.html")
+        context = {
+            "user_type": request.GET["user"],
+        }
+        print(context)
+        return render(request, "account/login.html", context)
     
     elif request.method == "POST":
         users_id = request.POST["users_id"]
@@ -26,12 +31,29 @@ def login(request):
             return render_login(request, user)
         else:
             messages.error(request, "Invalid username and/or password.")
-            return render(request, "login.html")
-    
+            return render(request, "account/login.html")
+    return render(request, "account/login.html")
+
 def render_login(request, user):
     context = {
        user: user,
+      # add
     }
     
-    return render(request, "home.html", context)
+    return render(request, "home/home.html", context)
     
+def signup(request):
+    if request.method == "GET":
+        signupForm = SignUpForm()
+        return render(request, "account/signup.html")
+    
+    elif request.method == "POST":
+        signupForm = SignUpForm(request.POST)
+        
+        if signupForm.is_valid():
+            signupForm.save()
+            messages.success(request, "Account created successfully.")
+            return redirect("home")
+        else:
+            messages.error(request, "Invalid form.")
+            return render(request, "account/signup.html")
