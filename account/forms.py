655fd24615ef06ebfa484925ea82dbb476f079a7
party <6410685215@student.tu.ac.th>
2023-11-10 20:10:26 +0700

    Update URLs and models for account app

--- account/forms.py
+++ account/forms.py
@@ -1,5 +1,32 @@
 from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from .models import User
+    
+class SignUpForm(UserCreationForm):
+    first_name = forms.CharField(max_length=30, required=True)
+    last_name = forms.CharField(max_length=30, required=True)
+    is_staff = True
+    
+    class Meta:
+        model = User
+        fields = ["username", "first_name", "last_name", "password1", "password2"]
+        
+    def save(self, commit=True):
+        user = super(SignUpForm, self).save(commit=False)
+        user.users_id = self.generate_users_id()
+        user.username = self.cleaned_data["username"]
+        user.first_name = self.cleaned_data["first_name"]
+        user.last_name = self.cleaned_data["last_name"]
+        user.is_staff = self.is_staff
+        
+        if commit:
+            user.save()
+            
+        return user
 
-class LoginForm(forms.Form):
-    users_id = forms.CharField(label="User ID", max_length=20)
-    password = forms.CharField(label="Password", max_length=20, widget=forms.PasswordInput)
+    def generate_users_id(self):
+        """
+        Generates a unique users_id for a new user.
+        """
+        users_id = f"{User.objects.count() + 1:06d}" 
+        return users_id
--- account/models.py
+++ account/models.py
@@ -8,6 +8,7 @@ class User(AbstractUser):
     Includes an additional `users_id` field that serves as the primary key.
     """
     users_id = models.CharField(max_length=20, primary_key=True)
+    username = models.CharField(max_length=20, unique=True)
 
     class Meta(AbstractUser.Meta):
         swappable = 'AUTH_USER_MODEL'
