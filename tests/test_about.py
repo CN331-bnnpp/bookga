b2d43a8e0972821489bf9b55ef8642310b8048fc
Pitchakan Leephud <6410615113@cn103>
2023-11-10 12:22:41 +0700

    add test_aboout

---
+++ tests/test_about.py
@@ -0,0 +1,36 @@
+from django.test import TestCase
+from django.urls import reverse
+
+class TestUrls(TestCase):
+    def test_about_url(self):
+        url = reverse('about')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'about/page_about.html')  
+
+    def test_home_url(self):
+        url = reverse('home')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'home/home.html')  
+
+    # def test_home_en_url(self):
+    #     # url = reverse('home', kwargs={'language_code': 'en'})
+    #     url = reverse('home')
+    #     self.assertEqual(url, '/en/')
+    #     response = self.client.get(url)
+    #     self.assertEqual(response.status_code, 200)
+    #     self.assertTemplateUsed(response, 'home/home.html')  
+
+class TestViews(TestCase):
+    def test_about_view(self):
+        url = reverse('about')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'about/page_about.html')
+
+    def test_home_view(self):
+        url = reverse('home')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'home/home.html')
