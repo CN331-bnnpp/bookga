753d8e2eaec51939a4a0baf9c11a28e6855b68a2
party <6410685215@student.tu.ac.th>
2023-11-11 22:21:32 +0700

    Add logout functionality and update login page heading

--- account/urls.py
+++ account/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('login/', views.login_app, name='login'),
+    path('logout/', views.logout_app, name='logout'),
     path('signup/', views.signup, name='signup'),
     path('create/', views.create_member, name='create'),
 ]
