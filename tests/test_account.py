0ab0819197abcf43c4a8086f7d49f55955736bf2
Pitchakan Leephud <6410615113@cn103>
2023-11-12 20:07:55 +0700

    update test_account

--- tests/test_account.py
+++ tests/test_account.py
@@ -54,11 +54,11 @@ class TestView(TestCase):
 
         self.assertTemplateUsed(response, 'account/types.html')
 
-    # def test_authenticated_user_redirect(self):
-    #     # Log in the user
-    #     self.client.login(username='testuser', password='testpassword')
-    #     response = self.client.get(reverse('/login'))
-    #     self.assertTemplateUsed(response, 'account/user.html')
+    def test_authenticated_user_redirect(self):
+        # Log in the user
+        self.client.login(username='testuser', password='testpassword')
+        response = self.client.get(reverse('/login'))
+        self.assertTemplateUsed(response, 'account/user.html')
 
     def test_get_request(self):
         # Make a GET request to login_app view
@@ -66,136 +66,136 @@ class TestView(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'account/login.html')
     
-#     def test_valid_login(self):
-#         # Make a POST request to login_app view with valid credentials
-#         response = self.client.post(reverse('login'), {'username': 'testuser', 'password': 'testpassword'})
-#         self.assertTemplateUsed(response, 'account/user.html')
+    def test_valid_login(self):
+        # Make a POST request to login_app view with valid credentials
+        response = self.client.post(reverse('login'), {'username': 'testuser', 'password': 'testpassword'})
+        self.assertTemplateUsed(response, 'account/user.html')
 
-#     def test_invalid_login(self):
-#         # Make a POST request to login_app view with invalid credentials
-#         response = self.client.post(reverse('login'), {'username': 'invaliduser', 'password': 'invalidpassword'})
-#         self.assertEqual(response.status_code, 200)
-#         self.assertContains(response, "Invalid username and/or password.")
+    def test_invalid_login(self):
+        # Make a POST request to login_app view with invalid credentials
+        response = self.client.post(reverse('login'), {'username': 'invaliduser', 'password': 'invalidpassword'})
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Invalid username and/or password.")
 
-#     def test_render_login_user(self):
-#         # Log in the user
-#         self.client.login(username='testuser', password='testpassword')
-#         response = self.client.get(reverse('login'), {'user': 'testuser'})
-#         self.assertTemplateUsed(response, 'account/user.html')
+    def test_render_login_user(self):
+        # Log in the user
+        self.client.login(username='testuser', password='testpassword')
+        response = self.client.get(reverse('login'), {'user': 'testuser'})
+        self.assertTemplateUsed(response, 'account/user.html')
 
-#     def test_render_login_staff(self):
-#         # Set the user as staff
-#         self.user.is_staff = True
-#         self.user.save()
-#         self.client.login(username='testuser', password='testpassword')
-#         response = self.client.get(reverse('login'), {'user': 'testuser'})
-#         self.assertTemplateUsed(response, 'account/staff.html')
+    def test_render_login_staff(self):
+        # Set the user as staff
+        self.user.is_staff = True
+        self.user.save()
+        self.client.login(username='testuser', password='testpassword')
+        response = self.client.get(reverse('login'), {'user': 'testuser'})
+        self.assertTemplateUsed(response, 'account/staff.html')
     
-#     def test_render_login_admin(self):
-#         # Set the user as admin
-#         self.user.is_superuser = True
-#         self.user.save()
-#         self.client.login(username='testuser', password='testpassword')
-#         response = self.client.get(reverse('login'), {'user': 'testuser'})
-#         self.assertTemplateUsed(response, 'account/admin.html')
+    def test_render_login_admin(self):
+        # Set the user as admin
+        self.user.is_superuser = True
+        self.user.save()
+        self.client.login(username='testuser', password='testpassword')
+        response = self.client.get(reverse('login'), {'user': 'testuser'})
+        self.assertTemplateUsed(response, 'account/admin.html')
 
-#     def test_get_signup_form(self):
-#         # Make a GET request to signup view
-#         response = self.client.get(reverse('signup'))
-#         self.assertEqual(response.status_code, 200)
-#         self.assertTemplateUsed(response, 'account/signup.html')
-#         self.assertIsInstance(response.context['form'], UserCreationForm)
+    def test_get_signup_form(self):
+        # Make a GET request to signup view
+        response = self.client.get(reverse('signup'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'account/signup.html')
+        self.assertIsInstance(response.context['form'], UserCreationForm)
 
-# class SignupViewTest(TestCase):
-#     def setUp(self):
-#         self.factory = RequestFactory()
+class SignupViewTest(TestCase):
+    def setUp(self):
+        self.factory = RequestFactory()
 
-#     def test_get_request(self):
-#         request = self.factory.get(reverse('signup'))
-#         response = signup(request)
+    def test_get_request(self):
+        request = self.factory.get(reverse('signup'))
+        response = signup(request)
 
-#         self.assertEqual(response.status_code, 200)
-#         self.assertContains(response, '<form')
-#         self.assertContains(response, 'name="username"')
-#         self.assertContains(response, 'name="password1"')
-#         self.assertContains(response, 'name="password2"')
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, '<form')
+        self.assertContains(response, 'name="username"')
+        self.assertContains(response, 'name="password1"')
+        self.assertContains(response, 'name="password2"')
 
-#     # def test_signup_view_POST_valid_form(self):
+    # def test_signup_view_POST_valid_form(self):
 
-#     # def test_signup_view_POST_invalid_form(self):
+    # def test_signup_view_POST_invalid_form(self):
 
-# class GroupModelTest(TestCase):
-#     @classmethod
-#     def setUpTestData(cls):
-#         # Create a user with is_staff set to True
-#         cls.staff_user = AccountUser.objects.create_user(username='staff_user', password='password', is_staff=True)
+class GroupModelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        # Create a user with is_staff set to True
+        cls.staff_user = AccountUser.objects.create_user(username='staff_user', password='password', is_staff=True)
 
-#     def setUp(self):
-#         # Create a group for testing
-#         self.test_group = group.objects.create(username=self.staff_user, group_name='TestGroup')
+    def setUp(self):
+        # Create a group for testing
+        self.test_group = group.objects.create(username=self.staff_user, group_name='TestGroup')
 
-#     def test_group_constraints(self):
-#         # Test that the group_name cannot be empty
-#         empty_group = group(username=self.staff_user, group_name='')
+    def test_group_constraints(self):
+        # Test that the group_name cannot be empty
+        empty_group = group(username=self.staff_user, group_name='')
         
-#         with self.assertRaises(ValidationError) as cm:
-#             empty_group.full_clean()
+        with self.assertRaises(ValidationError) as cm:
+            empty_group.full_clean()
 
-#         error_dict = cm.exception.error_dict
-#         self.assertIn('group_name', error_dict)
-#         self.assertEqual(error_dict['group_name'][0].code, 'blank')
+        error_dict = cm.exception.error_dict
+        self.assertIn('group_name', error_dict)
+        self.assertEqual(error_dict['group_name'][0].code, 'blank')
 
-# class CreateMemberViewTest(TestCase):
-#     def setUp(self):
-#         self.factory = RequestFactory()
-#         self.client = Client()
+class CreateMemberViewTest(TestCase):
+    def setUp(self):
+        self.factory = RequestFactory()
+        self.client = Client()
 
-#     def test_create_member_view_GET(self):
-#         response = self.client.get(reverse('create'))
+    def test_create_member_view_GET(self):
+        response = self.client.get(reverse('create'))
 
-#         self.assertEqual(response.status_code, 200)
-#         self.assertTemplateUsed(response, 'account/create_member.html')
-#         self.assertIn('form', response.context)
-#         self.assertIsInstance(response.context['form'], UserCreationForm)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'account/create_member.html')
+        self.assertIn('form', response.context)
+        self.assertIsInstance(response.context['form'], UserCreationForm)
 
-# class GroupMemberModelTest(TestCase):
-#     @classmethod
-#     def setUpTestData(cls):
-#         # Create a user with is_staff set to False
-#         cls.normal_user = AccountUser.objects.create_user(username='normal_user', password='password', is_staff=False)
+class GroupMemberModelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        # Create a user with is_staff set to False
+        cls.normal_user = AccountUser.objects.create_user(username='normal_user', password='password', is_staff=False)
 
-#         # Create a group
-#         cls.test_group = group.objects.create(username=cls.normal_user, group_name='TestGroup')
+        # Create a group
+        cls.test_group = group.objects.create(username=cls.normal_user, group_name='TestGroup')
 
-# class CreateAccountFormTest(TestCase):
-#     def test_save_method_creates_user(self):
-#         # Create form data
-#         form_data = {
-#             'username': 'testuser',
-#             'email': 'testuser@example.com',
-#             'password1': 'TestPassword123!',
-#             'password2': 'TestPassword123!',
-#         }
+class CreateAccountFormTest(TestCase):
+    def test_save_method_creates_user(self):
+        # Create form data
+        form_data = {
+            'username': 'testuser',
+            'email': 'testuser@example.com',
+            'password1': 'TestPassword123!',
+            'password2': 'TestPassword123!',
+        }
 
-#         form = CreateAccountForm(data=form_data)
-#         self.assertTrue(form.is_valid())
-#         user = form.save()
+        form = CreateAccountForm(data=form_data)
+        self.assertTrue(form.is_valid())
+        user = form.save()
 
-#         self.assertIsInstance(user, AccountUser)
-#         self.assertTrue(AccountUser.objects.filter(username='testuser').exists())
+        self.assertIsInstance(user, AccountUser)
+        self.assertTrue(AccountUser.objects.filter(username='testuser').exists())
 
-#     def test_save_method_does_not_commit_when_commit_is_false(self):
-#         # Create form data
-#         form_data = {
-#             'username': 'testuser',
-#             'email': 'testuser@example.com',
-#             'password1': 'TestPassword123!',
-#             'password2': 'TestPassword123!',
-#         }
-#         form = CreateAccountForm(data=form_data)
-#         self.assertTrue(form.is_valid())
-#         user = form.save(commit=False)
-#         self.assertIsInstance(user, AccountUser)
-#         self.assertFalse(AccountUser.objects.filter(username='testuser').exists())
-#         user.save()
-#         self.assertTrue(AccountUser.objects.filter(username='testuser').exists())
+    def test_save_method_does_not_commit_when_commit_is_false(self):
+        # Create form data
+        form_data = {
+            'username': 'testuser',
+            'email': 'testuser@example.com',
+            'password1': 'TestPassword123!',
+            'password2': 'TestPassword123!',
+        }
+        form = CreateAccountForm(data=form_data)
+        self.assertTrue(form.is_valid())
+        user = form.save(commit=False)
+        self.assertIsInstance(user, AccountUser)
+        self.assertFalse(AccountUser.objects.filter(username='testuser').exists())
+        user.save()
+        self.assertTrue(AccountUser.objects.filter(username='testuser').exists())
