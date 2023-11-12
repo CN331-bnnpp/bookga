0a2b816e9b5203914ca06f3b700dc41c71cf42b2
party <6410685215@student.tu.ac.th>
2023-11-12 14:50:58 +0700

    Update account models and admin, add account user setting, and clean home template

--- bookga/settings.py
+++ bookga/settings.py
@@ -134,3 +134,6 @@ if not DEBUG:
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# account user setting
+AUTH_USER_MODEL = 'account.AccountUser'
