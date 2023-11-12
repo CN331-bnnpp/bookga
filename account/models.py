69fcdb5507c2feafaddd2b90a80d6776e553b15a
party <6410685215@student.tu.ac.th>
2023-11-12 14:50:58 +0700

    Update account models and admin, add account user setting, and clean home template

--- account/models.py
+++ account/models.py
@@ -1,12 +1,17 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class AccountUser(AbstractUser):
+
+    class Meta(AbstractUser.Meta):
+        swappable = "AUTH_USER_MODEL"
+
 class group(models.Model):
     """
     A model representing a group of users.
     """
-    username = models.ForeignKey(User, on_delete=models.CASCADE, limit_choices_to={"is_staff": True})
+    username = models.ForeignKey(AccountUser, on_delete=models.CASCADE, limit_choices_to={"is_staff": True})
     group_name = models.CharField(max_length=20, primary_key=True)
         
     def __str__(self):
@@ -19,7 +24,7 @@ class group_member(models.Model):
     """
     A model representing a group member, linking a user to a group.
     """
-    username = models.ForeignKey(User, on_delete=models.CASCADE, limit_choices_to={"is_staff": False})
+    username = models.ForeignKey(AccountUser, on_delete=models.CASCADE, limit_choices_to={"is_staff": False})
     group_name = models.ForeignKey(group, on_delete=models.CASCADE)
     
     def __str__(self):
