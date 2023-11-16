20049c985772e10e80497fcba69241f1248f3182
party <6410685215@student.tu.ac.th>
2023-11-16 20:21:26 +0700

    Remove unused test files and update STATICFILES_DIRS in settings.py

--- bookga/settings.py
+++ bookga/settings.py
@@ -123,7 +123,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-# STATICFILES_DIRS = ['templates/',os.path.join(BASE_DIR, 'static'), 'public/']
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles')]
 if not DEBUG:
     STATIC_ROOT = os.path.join(BASE_DIR, 'static')
