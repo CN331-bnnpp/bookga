5c02105bc95efd0e5878dd6595f5fc50e0790427
party <6410685215@student.tu.ac.th>
2023-11-18 23:17:15 +0700

    Revert "Fix formatting issues in views.py and templates (TEMP)"
    This reverts commit 3f21661bd72d2342b58367d3a0ca47b5b58bbf94.

--- shift/views.py
+++ shift/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render ,redirect
+from django.shortcuts import render
 from .forms import ShiftForm ,BookingForm
 from .models import Shift, ShiftUser
 from account.models import group, group_member
@@ -18,21 +18,7 @@ def add_shift(request):
         if form.is_valid():
             form.instance.group_name = group.objects.get(username=request.user)
             form.save()
+            
             return render(request, "shift/add.html", context)
         
-    print(table.values_list('start_time', flat=True)[0])
     return render(request, "shift/add.html", context)
-
-
-
-
-def shifts_view(request):
-    user_group = group_member.objects.filter(username=request.user).first()
-
-    if user_group:
-        shifts = Shift.objects.filter(group_name=user_group.group_name)
-
-        return render(request, 'shift/shifts.html', {'shifts': shifts})
-    else:
-        print('error no group')
-        return render(request, 'shift/shifts.html')
