0e1a4cc889726b11c85337830baeac86253fc671
party <6410685215@student.tu.ac.th>
2023-11-19 21:03:46 +0700

    Revert "Add new URLs and templates for shift scheduling"

--- shift/urls.py
+++ shift/urls.py
@@ -3,6 +3,4 @@ from . import views
 
 urlpatterns = [
     path('add/', views.add_shift, name='add'),
-    path('schedule/', views.shift_schedule, name='schedule'),
-    path('book/', views.lookup_shift, name='book'),
 ]
--- tests/test_shift.py
+++ tests/test_shift.py
@@ -3,8 +3,7 @@ from django.urls import reverse, resolve
 from datetime import datetime, timezone
 from shift import views
 from shift.models import Shift, ShiftUser, group, AccountUser
-from shift.views import add_shift, shift_schedule, lookup_shift
-from account.models import group, group_member
+from shift.views import add_shift
 
 class TestShiftModels(TestCase):
     def setUp(self):
@@ -61,10 +60,6 @@ class TestUrls(TestCase):
     def test_add_shift_url_resolves(self):
         url = reverse('add')
         self.assertEqual(resolve(url).func, views.add_shift)
-        url = reverse('schedule')
-        self.assertEqual(resolve(url).func, views.shift_schedule)
-        url = reverse('book')
-        self.assertEqual(resolve(url).func, views.lookup_shift)
 
 class TestAddShiftView(TestCase):
     def setUp(self):
@@ -100,47 +95,3 @@ class TestAddShiftView(TestCase):
         self.assertEqual(response.status_code, 200)
 
         self.assertFalse(Shift.objects.exists())
-        
-class TestLookupShiftView(TestCase):
-    def setUp(self):
-        self.factory = RequestFactory()
-        self.sample_user = AccountUser.objects.create(username='test_user', password="testuser", is_staff=True)
-        self.sample_user2 = AccountUser.objects.create(username='test_user2', password="testuser2", is_staff=False)
-        self.sample_group = group.objects.create(username=self.sample_user, group_name="TestGroup")
-        self.start_time_utc = datetime(2023, 11, 18, 8, 0, tzinfo=timezone.utc)
-        self.shift = Shift.objects.create(
-            group_name=self.sample_group,
-            start_time=self.start_time_utc,
-            num_hours=4,
-            num_people=2
-        )
-        self.group_member = group_member.objects.create(username=self.sample_user2, group_name=self.sample_group)
-        self.shift_user = ShiftUser.objects.create(shift_id=self.shift, user_id=self.sample_user)
-        
-    def test_lookup_shift_view_with_valid_data(self):
-        data = {
-            'start_time': self.start_time_utc,
-            'num_hours': 4,
-            'num_people': 2
-        }
-
-        request = self.factory.post('/book/', data)
-        request.user = self.sample_user
-        response = shift_schedule(request)
-        response2 = lookup_shift(request)
-
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(response2.status_code, 200)
-        self.assertTrue(Shift.objects.exists())
-        
-        
-        request.user = self.sample_user2
-        response = shift_schedule(request)
-        response2 = lookup_shift(request)
-        
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(response2.status_code, 200)
-        self.assertTrue(Shift.objects.exists())
-        
-        
-        
