20e57b6a22b58ce4adde53c03f4213fbb82c3d8b
Pitchakan Leephud <6410615113@cn103>
2023-11-25 21:32:03 +0700

    complete tests (about, account, shift)

--- tests/test_about.py
+++ tests/test_about.py
@@ -1,27 +1,31 @@
 from django.test import TestCase
 from django.urls import reverse
 
-class TestUrls(TestCase):
+class AboutUrlsTest(TestCase):
     def test_about_url(self):
+        # test url path - about/page_abount.html
         url = reverse('about')
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'about/page_about.html')  
-
+    
     def test_home_url(self):
+        # test url path - home/home.html
         url = reverse('home')
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'home/home.html')  
 
-class TestViews(TestCase):
+class AboutViewsTest(TestCase):
     def test_about_view(self):
+        # test function about in about/views.py
         url = reverse('about')
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'about/page_about.html')
 
     def test_home_view(self):
+        # test function home in about/views.py
         url = reverse('home')
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
--- tests/test_shift.py
+++ tests/test_shift.py
@@ -3,17 +3,35 @@ from django.contrib import messages
 from django.urls import reverse, resolve
 from datetime import datetime, timezone
 from shift import views
-from shift.models import Shift, ShiftUser, group, AccountUser
 from shift.views import add_shift, shift_schedule, lookup_shift, book_shift
+from shift.models import Shift, ShiftUser, group, AccountUser
 from account.models import group, group_member
 
-class TestShiftModels(TestCase):
+class ShiftUrlsTest(TestCase):
+    def test_add_url_resolves(self):
+        # test url path - shift/add.html
+        url = reverse('add')
+        self.assertEqual(resolve(url).func, views.add_shift)
+
+    def test_schedule_url_resolves(self):
+        # test url path - shift/schedule.html
+        url = reverse('schedule')
+        self.assertEqual(resolve(url).func, views.shift_schedule)
+    
+    def test_book_url_resolves(self):
+        # test url path - shift/book.html
+        url = reverse('book')
+        self.assertEqual(resolve(url).func, views.lookup_shift)
+
+class ShiftModelsTest(TestCase):
     def setUp(self):
+        # create user, group and set up start time
         self.sample_user = AccountUser.objects.create(username='test_user', password="testuser",)
         self.sample_group = group.objects.create(username=self.sample_user, group_name="TestGroup")
         self.start_time_utc = datetime(2023, 11, 18, 8, 0, tzinfo=timezone.utc)
 
     def test_shift_creation(self):
+        # test that staff can crate shift
         shift = Shift.objects.create(
             group_name=self.sample_group,
             start_time=self.start_time_utc,
@@ -44,6 +62,7 @@ class TestShiftModels(TestCase):
         self.assertEqual(saved_shift_user.user_id, self.sample_user)
 
     def test_cascade_deletion(self):
+        # test that staff can delete shift
         shift = Shift.objects.create(
             group_name=self.sample_group,
             start_time=self.start_time_utc,
@@ -58,23 +77,16 @@ class TestShiftModels(TestCase):
         with self.assertRaises(ShiftUser.DoesNotExist):
             ShiftUser.objects.get(id=shift_user.id)
 
-class TestUrls(TestCase):
-    def test_add_shift_url_resolves(self):
-        url = reverse('add')
-        self.assertEqual(resolve(url).func, views.add_shift)
-        url = reverse('schedule')
-        self.assertEqual(resolve(url).func, views.shift_schedule)
-        url = reverse('book')
-        self.assertEqual(resolve(url).func, views.lookup_shift)
-
 class TestAddShiftView(TestCase):
     def setUp(self):
+        # create user, group and set up start time
         self.factory = RequestFactory()
         self.sample_user = AccountUser.objects.create(username='test_user', password="testuser",)
         self.sample_group = group.objects.create(username=self.sample_user, group_name="TestGroup")
         self.start_time_utc = datetime(2023, 11, 18, 8, 0, tzinfo=timezone.utc)
 
     def test_add_shift_view_with_valid_data(self):
+        # test add funtion in shift/views.py with valid data
         data = {
             'start_time': self.start_time_utc,
             'num_hours': 4,
@@ -91,6 +103,7 @@ class TestAddShiftView(TestCase):
         self.assertTrue(Shift.objects.exists())
 
     def test_add_shift_view_with_invalid_data(self):
+        # test add funtion in shift/views.py with invalid data
         data = {}
 
         request = self.factory.post('/add_shift/', data)
@@ -119,6 +132,7 @@ class LookupShiftViewTest(TestCase):
         self.shift_user = ShiftUser.objects.create(shift_id=self.shift, user_id=self.sample_user2)
 
     def test_lookup_shift_for_staff_user(self):
+        # test lookup_shift funtion in shift/views.py for staff
         request = self.factory.get('/lookup_shift/')
         request.user = self.sample_user
 
@@ -128,6 +142,7 @@ class LookupShiftViewTest(TestCase):
         self.assertIn('container', rendered_content)
         
     def test_lookup_shift_for_non_staff_user(self):
+        # test lookup_shift funtion in shift/views.py for non-staff
         request = self.factory.get('/lookup_shift/')
         request.user = self.sample_user2
 
@@ -137,6 +152,7 @@ class LookupShiftViewTest(TestCase):
         self.assertIn('container', rendered_content)
 
     def test_lookup_shift_post_request(self):
+        # test lookup_shift funtion in shift/views.py by post method request
         data = {
             'start_time': self.start_time_utc,
             'num_hours': 4,
@@ -144,7 +160,7 @@ class LookupShiftViewTest(TestCase):
         }
         request = self.factory.post('/lookup_shift/', data)
         request.user = self.sample_user
-        
+
         response = lookup_shift(request)
         
 class ShiftScheduleViewsTest(TestCase):
@@ -164,12 +180,14 @@ class ShiftScheduleViewsTest(TestCase):
         self.shift_user = ShiftUser.objects.create(shift_id=self.shift, user_id=self.sample_user)
 
     def test_shift_schedule_for_staff_user(self):
+        # test shift_schedule funtion in shift/views.py for staff
         request = self.factory.get('/shift_schedule/')
         request.user = self.sample_user
         response = shift_schedule(request)
         self.assertEqual(response.status_code, 200)
 
     def test_shift_schedule_for_non_staff_user(self):
+        # test shift_schedule funtion in shift/views.py for non-staff
         request = self.factory.get('/shift_schedule/')
         request.user = self.sample_user2
         response = shift_schedule(request)
@@ -190,12 +208,13 @@ class BookShiftViewsTest(TestCase):
         self.group_member = group_member.objects.create(username=self.sample_user, group_name=self.sample_group)
     
     def test_book_shift_success(self):
+        # test book_shift funtion in shift/views.py for book success
         request = self.factory.post('/book/', {'id': self.shift.pk})
         request.user = self.sample_user
 
         messages_to_assert = []
 
-        # in this scenario is not error. So mock_messages_error isn't work
+        # # in this scenario is not error. So mock_messages_error isn't work
         # def mock_messages_error(request, message):
         #     messages_to_assert.append(message)
         # messages.error = mock_messages_error
@@ -206,6 +225,7 @@ class BookShiftViewsTest(TestCase):
         self.assertEqual(len(messages_to_assert), 0)
     
     def test_book_shift_already_booked(self):
+        # test book_shift funtion in shift/views.py for book that already booked
         ShiftUser.objects.create(shift_id=self.shift, user_id=self.sample_user)
         
         request = self.factory.post('/book/', {'id': self.shift.pk})
@@ -223,6 +243,7 @@ class BookShiftViewsTest(TestCase):
         self.assertIn("You have already booked this shift", messages_to_assert)
     
     def test_book_shift_full(self):
+        # test book_shift funtion in shift/views.py for shift is full
         ShiftUser.objects.create(shift_id=self.shift, user_id=AccountUser.objects.create(username='user1'))
         ShiftUser.objects.create(shift_id=self.shift, user_id=AccountUser.objects.create(username='user2'))
 
